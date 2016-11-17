80e21517c42a53cf2536516b6c31e6127c1d4d68
William Notowidagdo <wnotowidagdo@gmail.com>
2016-11-17 17:30:12 +0700

    add title is presence validation to article model to ensure that article have a title

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'invalid without title' do
+    article = Article.new
+    refute article.valid?
+    assert_not_nil article.errors[:title]
+  end
 end
