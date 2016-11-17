ac9f9e93bed52251ec25db3ee50cad8e50096a4b
William Notowidagdo <wnotowidagdo@gmail.com>
2016-11-17 15:39:35 +0700

    add setup and teardown method ke articles controller

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,6 +1,14 @@
 require 'test_helper'
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @article = articles(:one)
+  end
+
+  teardown do
+    Rails.cache.clear
+  end
+
   test 'should get new' do
     get new_article_url
     assert_response :success
@@ -8,15 +16,19 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
 
   test 'should create article' do
     assert_difference('Article.count') do
-      post articles_url, params: { article: { body: 'Rails is awesome!', title: 'Hello Rails' } }
+      post articles_url, params: { article: { text: @article.text, title: @article.title } }
     end
 
     assert_redirected_to article_path(Article.last)
   end
 
   test 'should show article' do
-    article = articles(:one)
-    get article_url(article)
+    get article_url(@article)
+    assert_response :success
+  end
+
+  test 'should get index' do
+    get articles_url
     assert_response :success
   end
 end
