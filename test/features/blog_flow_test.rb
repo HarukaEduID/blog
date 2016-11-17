28ae7c02e920cd3b9657c465563a55e7ec3b498d
William Notowidagdo <wnotowidagdo@gmail.com>
2016-11-17 17:13:45 +0700

    add back links on new and show article page

--- test/features/blog_flow_test.rb
+++ test/features/blog_flow_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class BlogFlowTest < Capybara::Rails::TestCase
+  setup do
+    @article = articles(:one)
+  end
+
   test 'can access all articles list' do
     visit root_path
     click_link 'My Blog'
@@ -11,5 +15,17 @@ class BlogFlowTest < Capybara::Rails::TestCase
     visit articles_path
     click_link 'New article'
     assert page.has_current_path? new_article_path
+  end
+
+  test 'can access articles list from new article form' do
+    visit new_article_path
+    click_link 'Back'
+    assert page.has_current_path? articles_path
+  end
+
+  test 'can access articles list from show article' do
+    visit article_path(@article)
+    click_link 'Back'
+    assert page.has_current_path? articles_path
   end
 end
